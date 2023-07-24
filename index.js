ac89140bb193c697ac5522ef9ac10584f305d24f
markhryt <hrytchakmark@gmail.com>
2023-07-24 13:32:26 -0400

    new styling and structure implemented

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
-
+var id = 'about';
+function myMove() {
+  var elem = document.getElementById(id);
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+myMove();
